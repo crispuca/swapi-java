0c6a26efe6a4b0d53531dfc576d021f66f197da1
crispuca <crispuca10@gmail.com>
2025-01-27 23:22:55 -0300

    models

---
+++ src/main/java/com/demo/swapijava/models/People.java
@@ -0,0 +1,58 @@
+package com.demo.swapijava.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class People implements Serializable {
+
+    @JsonProperty("name")
+    public String name;
+
+    @JsonProperty("birth_year")
+    public String birthYear;
+
+    @JsonProperty("eye_color")
+    public String eyeColor;
+
+    @JsonProperty("gender")
+    public String gender;
+
+    @JsonProperty("hair_color")
+    public String hairColor;
+
+    @JsonProperty("height")
+    public String height;
+
+    @JsonProperty("mass")
+    public String mass;
+
+    @JsonProperty("skin_color")
+    public String skinColor;
+
+    @JsonProperty("homeworld")
+    public String homeWorldUrl;
+
+    @JsonProperty("films")
+    public List<String> filmsUrls;
+
+    @JsonProperty("species")
+    public List<String> speciesUrls;
+
+    @JsonProperty("starships")
+    public List<String> starshipsUrls;
+
+    @JsonProperty("vehicles")
+    public List<String> vehiclesUrls;
+
+    @JsonProperty("url")
+    public String url;
+
+    @JsonProperty("created")
+    public String created;
+
+    @JsonProperty("edited")
+    public String edited;
+
+}
